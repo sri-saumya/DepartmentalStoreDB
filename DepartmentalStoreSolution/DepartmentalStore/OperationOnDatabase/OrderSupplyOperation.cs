fdbfafc037298ff552bf10fab5c76077e6560b9e
Saumya Srivastava <saumya@git.com>
2021-05-21 09:03:24 -0700

    Using Entity Frame work

---
+++ DepartmentalStoreSolution/DepartmentalStore/OperationOnDatabase/OrderSupplyOperation.cs
@@ -0,0 +1,41 @@
+﻿using Store.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DepartmentalStore.OperationOnDatabase
+{
+    public class OrderSupplyOperation
+    {
+        public static StoreContext context = new StoreContext();
+        public static void BySupplierName()
+        {
+            Console.WriteLine("Query8");
+            var res = context.Supplier.Join(context.Product,
+                             e1 => e1.Id,
+                             e3 => e3.Id,
+                             (e1, e3) => new {
+                                 supp = e1.FirstName,
+                                 supp2 = e1.LastName,
+                                 prod = e3.ProductName,
+                                 prod2 = e3.Manufacturer
+                             });
+            Console.WriteLine("FirstName" + "\t\t"+"LastName" + "\t\t" + "ProductName" + "\t\t" + "Manufacturer \n");
+            foreach (var i in res)
+            {
+                Console.WriteLine($"{ i.supp} \t\t{ i.supp2} \t\t{ i.prod} \t\t { i.prod2}");
+            }
+        }
+
+        public static void SupplyAfterParticularDate()
+        {
+
+        }
+
+        public static void SupplyBeforeParticularDate()
+        {
+
+        }
+    }
+}
--- DepartmentalStoreSolution/DepartmentalStore/Program.cs
+++ DepartmentalStoreSolution/DepartmentalStore/Program.cs
@@ -26,12 +26,14 @@ namespace DepartmentalStore
             while (true)
             {
 
-                Console.WriteLine("Select Query type(1,2,3) :1)Query on staff\n \t2)Query on product \n \t3)Query on Supplier");
+                Console.WriteLine("Select Query type(1,2,3) :1)Query on staff\n \t2)Query on product \n \t3)Query on Supplier\n \t4)Query on Order and Supply");
                 var userChoice = Console.ReadLine();
-                if (userChoice == "2") { Query.QueryOnProduct(); }
-                if (userChoice == "1") { Query.QueryOnStaff(); }
-                if (userChoice == "3") { Query.QueryOnSupplier(); }
-                  
+                if (userChoice == "1") { UserInputForQuery.QueryOnStaff(); }
+                if (userChoice == "2") { UserInputForQuery.QueryOnProduct(); }
+                if (userChoice == "3") { UserInputForQuery.QueryOnSupplier(); }
+                if (userChoice == "4") { UserInputForQuery.QueryOnOrder(); }
+
+
 
             }
 
